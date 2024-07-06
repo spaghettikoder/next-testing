be1c1409861a9a388a0e3713635abef6f188edda
spaghettikoder <a.meslmani@innopolis.university>
2024-07-06 23:04:41 +0300

    kk

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,21 @@
 
 
 const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
+  basePath: '/your-repository-name',
+  assetPrefix: '/your-repository-name/',
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/comic': { page: '/comic' },
+    };
+  },
 };
 
 export default nextConfig;
