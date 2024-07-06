3484ab8551d64b18030071fb607c4f66baa7d8de
spaghettikoder <a.meslmani@innopolis.university>
2024-07-06 18:49:44 +0300

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/your-repo-name',
+    assetPrefix: '/your-repo-name/',
+  };
+  
+  export default nextConfig;
