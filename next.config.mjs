a47ea281d8535e2dfd0e87a7e1231d2b4c3b7860
spaghettikoder <a.meslmani@innopolis.university>
2024-07-06 20:16:41 +0300

    tak2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/next-testing",
   output: "export",
-  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
